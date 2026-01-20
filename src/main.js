4fffc85116b15b0576db71a37555e6f8758ccad2
vitor gamarano <146540200+Vitor-S-G-C@users.noreply.github.com>
2026-01-20 09:47:37 -0300

    Update message and footer in main.js20012026

--- src/main.js
+++ src/main.js
@@ -7,11 +7,11 @@ document.querySelector('#app').innerHTML = `
         <h1>Uma cartinha fofa</h1>
       </header>
         <section class="cartinha-body">
-          <p class="message">Vim por meio desta cartinha pedir um favor: poderia, por obséquio,<br/>me passar uma cópia do banco de dados para que eu realize alguns<br/>testes de performance? ✨</p>
+          <p class="message">Vim por meio desta cartinha pedir um favor: poderia, por obséquio,<br/>me passar uma cópia do banco de dados, a mesma que eu havia falado que queria toda seguinda,<br/> não e sua culpa pois eu havia esquecida de pedir a mesma✨</p>
           <p class="details">Pequenos gestos fazem grandes dias — sorria e aproveite!</p>
         </section>
       <footer class="cartinha-footer">
-        <p class="from">Com carinho, <strong>Vitor</strong></p>
+        <p class="from">Com carinho, <strong>🐱Vitor🐱</strong></p>
         <button id="toggle" class="btn">Abrir / Fechar</button>
       </footer>
     </article>
